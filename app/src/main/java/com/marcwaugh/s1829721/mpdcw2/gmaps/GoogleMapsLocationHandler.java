6a04f5acca613f7da2095b769cfc725686e7506b
Marc8888 <47319244+Marc8888@users.noreply.github.com>
2020-04-20 01:07:40 +0100

    Refactored map logic

--- app/src/main/java/com/marcwaugh/s1829721/mpdcw2/gmaps/GoogleMapsLocationHandler.java
+++ app/src/main/java/com/marcwaugh/s1829721/mpdcw2/gmaps/GoogleMapsLocationHandler.java
@@ -16,65 +16,48 @@ package com.marcwaugh.s1829721.mpdcw2.gmaps;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Build;
-import android.os.Bundle;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.marcwaugh.s1829721.mpdcw2.listenerinterfaces.IApplicationPermissionResultListener;
 
 public class GoogleMapsLocationHandler
-		implements IApplicationPermissionResultListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener
+		implements IApplicationPermissionResultListener
 {
 	private static final int PERM_REQUEST_LOCATION = 99;
-
-	IFragmentLocationWantingMap mMap;
-	SupportMapFragment mapFrag;
-	LocationRequest mLocationRequest;
-	GoogleApiClient mGoogleApiClient;
-	Location mLastLocation;
+	IFragmentLocationWantingMap mMapFragment;
 
 	public GoogleMapsLocationHandler(IFragmentLocationWantingMap fragmentMap)
 	{
-		this.mMap = fragmentMap;
+		this.mMapFragment = fragmentMap;
 	}
 
 	public void onMapReady()
 	{
-		GoogleMap map = mMap.getGMapInstance();
+		GoogleMap map = mMapFragment.getGMapInstance();
 
 		//Initialize Google Play Services
 		if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
 		{
-			if (ContextCompat.checkSelfPermission(mMap.getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+			if (ContextCompat.checkSelfPermission(mMapFragment.getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
 					== PackageManager.PERMISSION_GRANTED)
 			{
-				//Location Permission already granted
-				buildGoogleApiClient();
+				// Location Permission already granted
 				map.setMyLocationEnabled(true);
 			}
 			else
 			{
-				//Request Location Permission
+				// Request Location Permission
 				checkLocationPermission();
 			}
 		}
 		else
 		{
-			buildGoogleApiClient();
 			map.setMyLocationEnabled(true);
 		}
 	}
@@ -82,27 +65,27 @@ public class GoogleMapsLocationHandler
 
 	private void checkLocationPermission()
 	{
-		Context ctx = mMap.getContext();
+		Context ctx = mMapFragment.getContext();
 		if (ContextCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION)
 				!= PackageManager.PERMISSION_GRANTED)
 		{
 			// Should a permission explanation be displayed
 			//  eg    "Let this application show your location"
 			// why -> "We want to display your location on a route"
-			if (ActivityCompat.shouldShowRequestPermissionRationale(mMap.getMainActivity(),
+			if (ActivityCompat.shouldShowRequestPermissionRationale(mMapFragment.getMainActivity(),
 					Manifest.permission.ACCESS_FINE_LOCATION))
 			{
 
 				// Show an explanation to the user *asynchronously* -- don't block
 				// this thread waiting for the user's response! After the user
 				// sees the explanation, try again to request the permission.
-				new AlertDialog.Builder(mMap.getContext())
+				new AlertDialog.Builder(mMapFragment.getContext())
 						.setTitle("Location Permission Needed")
 						.setMessage("This app needs the Location permission, please accept to use location functionality")
 						.setPositiveButton("OK", (dialogInterface, i) ->
 						{
 							//Prompt the user once explanation has been shown
-							ActivityCompat.requestPermissions(mMap.getMainActivity(),
+							ActivityCompat.requestPermissions(mMapFragment.getMainActivity(),
 									new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
 									PERM_REQUEST_LOCATION);
 						})
@@ -112,98 +95,34 @@ public class GoogleMapsLocationHandler
 			else
 			{
 				// No explanation needed, we can request the permission.
-				ActivityCompat.requestPermissions(mMap.getMainActivity(),
+				ActivityCompat.requestPermissions(mMapFragment.getMainActivity(),
 						new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
 						PERM_REQUEST_LOCATION);
 			}
 		}
 	}
 
-	private synchronized void buildGoogleApiClient()
-	{
-		mGoogleApiClient = new GoogleApiClient.Builder(mMap.getContext())
-				.addConnectionCallbacks(this)
-				.addOnConnectionFailedListener(this)
-				.addApi(LocationServices.API)
-				.build();
-		mGoogleApiClient.connect();
-	}
-
-	@Override
-	public void onConnected(@Nullable Bundle bundle)
-	{
-		mLocationRequest = new LocationRequest();
-		mLocationRequest.setInterval(1000);
-		mLocationRequest.setFastestInterval(1000);
-		mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-
-		reloadLocation();
-	}
-
-	public void reloadLocation()
-	{
-		if (ContextCompat.checkSelfPermission(mMap.getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-				== PackageManager.PERMISSION_GRANTED)
-		{
-			FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(mGoogleApiClient.getContext());
-			fusedLocationClient.getLastLocation().addOnSuccessListener(this::onLocationSuccess);
-		}
-	}
-
-
-	private void onLocationSuccess(Location location)
-	{
-		if (location != null)
-		{
-			mLastLocation = location;
-		}
-	}
-
-	@Override
-	public void onConnectionSuspended(int i)
-	{
-		//
-	}
-
-	@Override
-	public void onConnectionFailed(@NonNull ConnectionResult connectionResult)
-	{
-		//
-	}
-
 	@Override
 	public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults)
 	{
-		switch (requestCode)
+		if (requestCode == PERM_REQUEST_LOCATION)
 		{
-			case PERM_REQUEST_LOCATION:
+			// If the results are empty then the request was denied,
+			//   if not check if our permission is granted
+			if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
 			{
-				// If request is cancelled, the result arrays are empty.
-				if (grantResults.length > 0
-						&& grantResults[0] == PackageManager.PERMISSION_GRANTED)
-				{
-					// permission was granted, yay! Do the
-					// location-related task you need to do.
-					if (ContextCompat.checkSelfPermission(mMap.getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-							== PackageManager.PERMISSION_GRANTED)
-					{
-						if (mGoogleApiClient == null)
-							buildGoogleApiClient();
-
-						mMap.getGMapInstance().setMyLocationEnabled(true);
-					}
-
-				}
-				else
-				{
-					// permission denied, boo! Disable the
-					// functionality that depends on this permission.
-					Toast.makeText(mMap.getContext(), "permission denied", Toast.LENGTH_LONG).show();
-				}
+				// Permission was granted
+				if (ContextCompat.checkSelfPermission(mMapFragment.getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+						== PackageManager.PERMISSION_GRANTED)
+					mMapFragment.getGMapInstance().setMyLocationEnabled(true);
 			}
+			else
+			{
+				// Permission has been denied, disable MyLocation.
+				mMapFragment.getGMapInstance().setMyLocationEnabled(false);
 
-			// other 'case' lines to check for other
-			// permissions this app might request
+				Toast.makeText(mMapFragment.getContext(), "Location permission denied", Toast.LENGTH_LONG).show();
+			}
 		}
 	}
 }
